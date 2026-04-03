6ac5eab7b9927d6fbd6dbbfd11f2f5859e572059
basketdestroyer <enkayem69@gmail.com>
2026-04-04 00:40:14 +0200

    fix: #10 update database settings to use environment variable for DB_HOST

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -14,6 +14,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,7 +87,7 @@ DATABASES = {
         'NAME': 'cardbid_db',
         'USER': 'admin',
         'PASSWORD': 'adminpassword',
-        'HOST': 'localhost',
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
         'PORT': '5432',
     }
 }
