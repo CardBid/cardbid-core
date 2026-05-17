f01c6c183a0e7413cff2b891aaa68514bc4131bb
RhobarIII <b.k.zygmunt@gmail.com>
2026-05-17 20:47:20 +0200

    feat: #26 - dokonczony

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -18,8 +18,7 @@ urlpatterns = [
     path("api/token/refresh/", TokenRefreshView.as_view(), name='token_refresh'),
 
     path("api/", include("auctions.urls")),
-    path('v1/auctions/<int:pk>/live-data/', AuctionLiveDataView.as_view(), name='auction-live-data'),
-    path('v1/auctions/<int:pk>/buy-now/', AuctionBuyNowView.as_view(), name='auction-buy-now'),
+    
 ]
 
 if settings.DEBUG:
