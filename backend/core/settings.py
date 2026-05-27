7821dfbc20993c9ac86e6cabb778da0b4325b940
EnKayEm <enkayem69@gmail.com>
2026-05-27 19:22:27 +0200

    fix: remove redundant Cloudinary configuration from settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -202,9 +202,6 @@ CLOUDINARY_STORAGE = {
 }
 
 cloudinary.config( 
-    cloud_name = os.environ.get('CLOUD_NAME'), 
-    api_key = os.environ.get('API_KEY'), 
-    api_secret = os.environ.get('API_SECRET'),
     secure = True
 )
 
