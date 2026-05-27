017259e780857f72789f810a509508c695ccf742
EnKayEm <enkayem69@gmail.com>
2026-05-27 13:37:10 +0200

    fix: update STATICFILES_DIRS path to reference the correct directory for static files

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -139,7 +139,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
-    BASE_DIR.parent / "static", 
+    BASE_DIR / "static", 
 ]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
