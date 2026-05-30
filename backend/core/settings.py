5c9353ff5515ec56d193ffef4d98bef9bad816bf
basketdestroyer <enkayem69@gmail.com>
2026-05-30 17:08:24 +0200

    feat: remove unnecessary SSL certificate requirement from Redis configuration

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -185,7 +185,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379') + "?ssl_cert_reqs=none"],
+            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
         },
     },
 }
